6c48456ae7b7b1e16cacd71670f6a5ffa2488920
Pedro.c <pedroogabrielg@gmail.com>
2026-05-18 00:46:02 -0300

    chore: migrate to bun package manager

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './api/src/db/schema.ts',
+  schema: './api/src/db/*.ts',
   out: './api/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL || '',
   },
+  verbose: true,
 });
